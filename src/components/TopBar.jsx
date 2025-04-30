45842d6a4d567532bdef4d5d775f208c60743236
juyeonBae <w2316@e-mirim.hs.kr>
2025-04-30 15:06:20 +0900

    fix : 간격 추가

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -7,7 +7,7 @@ export default function TopBar({ value, address }) {
         navigate(`${address}`)
     }
     return (
-        <div style={{ display: "flex", alignItems: "center", padding: "16px 32px" }}>
+        <div style={{ display: "flex", alignItems: "center", padding: "16px 32px", marginTop: "20px" }}>
             <img src={PrevBtn}
                 style={{ width: "15px", height: "25px", cursor: "pointer" }}
                 onClick={handleClick}
