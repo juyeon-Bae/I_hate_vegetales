413002da443f3cc578a2b4866705d451f0b08f54
juyeonBae <w2316@e-mirim.hs.kr>
2025-04-30 14:29:02 +0900

    feat : TopBar Component

---
+++ src/components/TopBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import PrevBtn from "../assets/images/prevBtn.png"
+export default function TopBar({ value, address }) {
+    const navigate = useNavigate();
+    const handleClick = () => {
+        navigate(`${address}`)
+    }
+    return (
+        <div style={{ display: "flex", alignItems: "center", padding: "16px 32px" }}>
+            <img src={PrevBtn}
+                style={{ width: "15px", height: "25px", cursor: "pointer" }}
+                onClick={handleClick}
+            />
+
+            <span style={{ textAlign: "center", margin: "auto", fontSize: "22px" }}>{value}</span>
+        </div>
+    )
+}
