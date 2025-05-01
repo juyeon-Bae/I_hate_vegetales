a7b119859dc418ee3b44f91318c5a87cba97dcde
juyeonBae <w2316@e-mirim.hs.kr>
2025-05-01 21:41:23 +0900

    fix : 버튼 역할 분리

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "../styles/Button.module.css"
-export default function Button({ value, address }) {
+export default function Button({ value, address, type }) {
     const navigate = useNavigate();
     const handleClick = () => {
-        navigate(`${address}`)
+        if (address) navigate(address)
     }
     return (
-        <div className={styles.btnContainer}
+        <button
+            type={type}
+            className={styles.btnContainer}
             onClick={handleClick}
         >
             <span className={styles.value}>{value}</span>
-        </div>
+        </button>
     )
 }
