6be375e434b0a0530c91d4541cdc4fcb72a239d2
juyeonBae <w2316@e-mirim.hs.kr>
2025-05-08 22:51:21 +0900

    fix : id, 삭제 함수 props 추가

--- src/components/ViewGoals.jsx
+++ src/components/ViewGoals.jsx
@@ -1,6 +1,6 @@
 import styles from "../styles/ViewGoals.module.css"
 import Alram from "../assets/images/alram.png"
-export default function ViewGoals({ content }) {
+export default function ViewGoals({ content, id, onDelete }) {
     return (
         <div className={styles.allContainer}>
             <div className={styles.contentContainer}>
@@ -8,7 +8,9 @@ export default function ViewGoals({ content }) {
                 <div style={{ width: "290px" }}>
                     <div className={styles.titleContainer}>
                         <p style={{ fontSize: "15px", fontWeight: 600 }}>목표 보기</p>
-                        <p style={{ fontSize: "12px", color: "#CCC" }}>삭제하기</p>
+                        <p
+                            onClick={() => onDelete(id)}
+                            style={{ fontSize: "12px", color: "#CCC" }}>삭제하기</p>
                     </div>
                     <p style={{ fontSize: "15px", marginTop: "7px" }}>{content}</p>
                 </div>
