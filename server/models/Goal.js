e3a884a68113f70d2445ab7702c3e38af2287b78
juyeonBae <w2316@e-mirim.hs.kr>
2025-05-08 10:07:45 +0900

    feat : goal Schema 구현

---
+++ server/models/Goal.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const goalSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+module.exports = mongoose.model('Goal', goalSchema);
